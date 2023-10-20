eabcb71f27842ea70b9b414ac915e9acf9920323
Jessica Burnett <jessicaleighburnett@gmail.com>
2023-10-20 17:24:21 -0400

    Housekeeping

--- data-raw/make_solicitationslookup.R
+++ data-raw/make_solicitationslookup.R
@@ -3,26 +3,29 @@
 #' A lookup table for ESD and ASP solicitations to easily index solicitations and proposals according to program name.
 #' 
 #' @docType data
-#' 
+#'
+#'
+#'
 
-fn <- list.files(path="data-raw", pattern="ESD-ASP-solicitations-lookup", full.names=TRUE, recursive=TRUE)
+fn <- list.files(pattern="asp-solicitation", full.names=TRUE, recursive=TRUE)
+stopifnot(length(fn)>0)
 names <- readxl::excel_sheets(fn)
 
 for(i in seq_along(names)){
   temp <- readxl::read_xlsx(fn, sheet = names[i]) |> 
     dplyr::mutate(`Program Name` = names[i])
-  if(i == 1) lookup <- temp
-  lookup <- dplyr::bind_rows(temp)
+  if(i == 1) sols_lookup <- temp
+  sols_lookup <- dplyr::bind_rows(temp)
   rm(temp)
 }
 
 ## munge colnames
-names(lookup) <- tolower(names(lookup))
+names(sols_lookup) <- tolower(names(sols_lookup))
 
-looku <- lookup |>
+sols_lookup <- sols_lookup |>
   dplyr::rename("proposal number" = 'proposal no')
 
 
-usethis::use_data(lookup)
+usethis::use_data(sols_lookup)
 
 
