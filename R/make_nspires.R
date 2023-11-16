18138a4d269cac1fa1b7826b8013ea80296033d0
Jessica Burnett <jessicaleighburnett@gmail.com>
2023-11-16 10:32:43 -0500

    Update make_nspires.R

--- R/make_nspires.R
+++ R/make_nspires.R
@@ -86,7 +86,7 @@ if(dplyr::is_grouped_df(people)) people <- dplyr::ungroup(people)
 nspires <- list()
 nspires$proposals <- as.data.frame(proposals)
 nspires$people <- as.data.frame(people)
-nspires$lookup <- as.data.frame(nasaprograminsights::sols_lookup)
+nspires$lookup <- as.data.frame(nasaprograminsights::lookup)
 
 
 
