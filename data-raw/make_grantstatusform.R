41d1d5451dd9c285271dc265a1d7650343e623c8
Jessica Burnett <jessicaleighburnett@gmail.com>
2023-12-21 17:09:00 -0500

    add anand packages

--- data-raw/make_grantstatusform.R
+++ data-raw/make_grantstatusform.R
@@ -5,15 +5,15 @@
 #' @author Jessica Burnett \email{jessica.burnett@nasa.gov}
 #' @references \url{https://www.nasa.gov/centers/nssc/forms/grant-status-form}
 #' @keywords data grants
-#' @description 
-#' This datatable contains multiple, hand-corrected and munged files from a public-facing source, Grant Status Form, for NASA grant award IDs. 
-#' Unfortunately, this data table is made by downloading Grant Status Form exports one at a time (as of 2023). 
+#' @description
+#' This datatable contains multiple, hand-corrected and munged files from a public-facing source, Grant Status Form, for NASA grant award IDs.
+#' Unfortunately, this data table is made by downloading Grant Status Form exports one at a time (as of 2023).
 NULL
 
 # specify the directory containing data and the NSPIRES imported information
 dir <- list.files("/Users/jlburne3/Library/CloudStorage/OneDrive-NASA/bdecprogrameval/data-raw", pattern="grantstatusform",  full.names = TRUE)
 
-# These files were __hand-munged__ b/c the grant status form only exports as .csv and many of the rows are riddled with errors. 
+# These files were __hand-munged__ b/c the grant status form only exports as .csv and many of the rows are riddled with errors.
 fns <- list.files(dir, pattern="awarddates", full.names = TRUE, recursive = TRUE)
 
 grantstatusform <- lapply(
