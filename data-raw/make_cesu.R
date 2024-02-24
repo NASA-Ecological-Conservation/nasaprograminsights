e462411a90b69c3e67c0513cd61b192c9cd6d2df
Jessica Burnett <jessicaleighburnett@gmail.com>
2024-02-24 14:37:50 -0500

    Fix cesu dataframe

--- data-raw/make_cesu.R
+++ data-raw/make_cesu.R
@@ -9,7 +9,13 @@
 fn <- list.files(pattern="cesu-partner", full.names=TRUE, recursive=TRUE, ignore.case = TRUE)
 stopifnot(length(fn)==1)
 cesu <- read.csv(fn, strip.white = TRUE, check.names=FALSE) |> dplyr::select(-Notes)
-cesu <- tidyr::pivot_longer(cesu, cols=1:ncol(cesu) ,names_to = "unit", values_to = "member")
+cesu <- tidyr::pivot_longer(cesu, cols=1:ncol(cesu) ,names_to = "unit", values_to = "member") 
+
+#remove blanks, nas
+cesu <- cesu[cesu$member!="",]
+
+cesu$unit <- as.factor(cesu$unit)
+cesu$member <- as.factor(cesu$member)
 
 # Export Data to Pkg ------------------------------------------------------
 usethis::use_data(cesu, overwrite=TRUE)
